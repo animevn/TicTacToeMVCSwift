db342cc3824dc8e98c9aa81d703cd26e4c63dad6
animevn <haanhth21@gmail.com>
2020-02-04 20:37:38 +0700

    algorithm ok

--- TicTacToe/Controllers/ViewController.swift
+++ TicTacToe/Controllers/ViewController.swift
@@ -2,15 +2,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uvBoard: UIView!
     @IBOutlet weak var lbInfo: UILabel!
+    var board:Board = Board()
+    var player:Player?
+    
+    func clearBoard(){
+        let subviews = uvBoard.subviews
+        var buttons:[UIButton] =  [UIButton]()
+        for subview in subviews{
+            let temp = subview.subviews.filter{$0 is UIButton}
+            temp.forEach{buttons.append(($0 as! UIButton))}
+        }
+        for button in buttons{
+            button.setTitle("", for: .normal)
+        }
+    }
+    
+    func handleLabelInfo(){
+        lbInfo.text = "\(board.currentPlayer.description) to play"
+    }
+    
+    func resetBoard(){
+        board.restart()
+        clearBoard()
+        handleLabelInfo()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = view.viewWithTag(11) as! UIButton
-        button.setTitle("test", for: .normal)
+        resetBoard()
     }
+    
+    func makeGameMove(button:UIButton){
+        guard let player = player else {return}
+        button.setTitle("\(player.description)", for: .normal)
+        if let winner = board.winner{
+            lbInfo.text = "\(winner.description) wins!!!"
+        }
+        if board.state == State.Draw{
+            lbInfo.text = "Draws!!!"
+        }
+    }
+    
+    func onButtonPressed(button:UIButton){
+        if board.state == State.InProgress{
+            let tag:String = String(button.tag)
+            guard let row = Int(String((Array(tag)[0]))),
+                let column = Int(String((Array(tag)[1]))) else {return}
+            player = board.makeOneMove(row: row - 1, column: column - 1)
+            handleLabelInfo()
+            makeGameMove(button: button)
+        }
+    }
+    
+    @IBAction func onButtonsPressed(_ sender: UIButton) {
+        onButtonPressed(button: sender)
+    }
+    
 
     @IBAction func onButtonNewGamePressed(_ sender: UIBarButtonItem) {
+        resetBoard()
     }
     
 }
